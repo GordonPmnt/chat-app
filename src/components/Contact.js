7190f66a41ae883dbca12c9880a19ca86add5e51
Gordon Preumont <preumontgr@gmail.com>
2020-03-30 20:27:29 +0200

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
-    return <p>Contact</p>
+    return (
+        <div className="Contact">
+            <img src="https://randomuser.me/api/portraits/women/22.jpg" className="avatar" alt="avatar" />
+            <div>
+                <h4 className="name">Catherine Castro</h4>
+                <div className="status">
+                    <div className="status-online"/>
+                    <p className="status-text" >online</p>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Contact;
